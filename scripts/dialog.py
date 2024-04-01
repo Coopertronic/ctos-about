9bd23925eb65b21c054e1322c5af3817bbc7bc21
Matthew Cooper <coopertronics@gmail.com>
2024-04-01 19:30:08 +0100

    Files seprated ready to make the pkgbuild.

---
+++ scripts/dialog.py
@@ -0,0 +1,27 @@
+# This Python file uses the following encoding: utf-8
+import sys
+
+from PySide6.QtWidgets import QApplication, QDialog
+
+# Important:
+# You need to run the following command to generate the ui_form.py file
+#     pyside6-uic form.ui -o ui_form.py, or
+#     pyside2-uic form.ui -o ui_form.py
+from ui_form import Ui_Dialog
+
+class Dialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+        self.ui.textBrowser.append('<h1>Hello World</h1>')
+        self.ui.textBrowser.append('This is a quick demo.')
+        self.ui.textBrowser.append('This is just text in a browser')
+        self.ui.textBrowser.append('A very simple browser')
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    widget = Dialog()
+    widget.show()
+    sys.exit(app.exec())
---
+++ scripts/ui_form.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'form.ui'
+##
+## Created by: Qt User Interface Compiler version 6.6.3
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QAbstractScrollArea, QApplication, QDialog, QGridLayout,
+    QGroupBox, QSizePolicy, QTextBrowser, QVBoxLayout,
+    QWidget)
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        if not Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
+        Dialog.resize(599, 517)
+        self.verticalLayout_2 = QVBoxLayout(Dialog)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.groupBox = QGroupBox(Dialog)
+        self.groupBox.setObjectName(u"groupBox")
+        self.verticalLayout = QVBoxLayout(self.groupBox)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.gridLayout = QGridLayout()
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.textBrowser = QTextBrowser(self.groupBox)
+        self.textBrowser.setObjectName(u"textBrowser")
+        self.textBrowser.setSizeAdjustPolicy(QAbstractScrollArea.AdjustToContents)
+
+        self.gridLayout.addWidget(self.textBrowser, 0, 0, 1, 1)
+
+
+        self.verticalLayout.addLayout(self.gridLayout)
+
+
+        self.verticalLayout_2.addWidget(self.groupBox)
+
+
+        self.retranslateUi(Dialog)
+
+        QMetaObject.connectSlotsByName(Dialog)
+    # setupUi
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Dialog", None))
+        self.groupBox.setTitle(QCoreApplication.translate("Dialog", u"GroupBox", None))
+    # retranslateUi
+
